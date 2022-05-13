25246c279a401f78c303e3725c5bba905f24247c
Nigel <nigelsmyth@hotmail.com>
2022-05-13 16:25:40 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import List from './components/List';
+import withListLoading from './components/withListLoading';
 function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+// this is from https://www.smashingmagazine.com/2020/06/rest-api-react-fetch-axios/
+
+  // this is using fetch
+ // useEffect(() => {
+//    setAppState({ loading: true });
+  //  const apiUrl = `https://api.github.com/users/nidge/repos`;
+  //  fetch(apiUrl)
+    //  .then((res) => res.json())
+      //.then((repos) => {
+    //    setAppState({ loading: false, repos: repos });
+   //   });
+ // }, [setAppState]);
+
+  // this is using axios
+useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = 'https://api.github.com/users/hacktivist123/repos';
+    axios.get(apiUrl).then((repos) => {
+      const allRepos = repos.data;
+      setAppState({ loading: false, repos: allRepos });
+    });
+  }, [setAppState]);
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <h1>My Repositories</h1>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <footer>
+        <div className='footer'>
+          Built{' '}
+          <span role='img' aria-label='love'>
+            💚
+          </span>{' '}
+          with by Shedrack Akintayo
+        </div>
+      </footer>
     </div>
   );
 }
-
 export default App;
