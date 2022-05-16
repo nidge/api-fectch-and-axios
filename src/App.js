1bd7f7b33458100c95c0be0543556fb77deca869
Nigel Smyth <nigel.smyth@leeds.gov.uk>
2022-05-16 10:49:56 +0100

    Added code to read api when button pushed

--- src/App.js
+++ src/App.js
@@ -10,6 +10,26 @@ function App() {
     repos: null,
   });
 
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+handleSubmit() {
+  useEffect(() => {
+    setAppState({ loading: true });
+   const apiUrl = 'https://api.github.com/users/nidge/repos';
+    axios.get(apiUrl).then((repos) => {
+      const allRepos = repos.data;
+      setAppState({ loading: false, repos: allRepos });
+      console.log(allRepos);
+    });
+  }, [setAppState]);
+
+}
+
+
+
 // this is from https://www.smashingmagazine.com/2020/06/rest-api-react-fetch-axios/
 
   // this is using fetch
@@ -35,6 +55,12 @@ useEffect(() => {
   
   
   return (
+
+
+    <form onSubmit={this.handleSubmit}>
+    <Container>
+
+
     <div className='App'>
       <div className='container'>
         <h1>My Repositories</h1>
@@ -52,6 +78,20 @@ useEffect(() => {
         </div>
       </footer>
     </div>
+
+
+
+        <Button type="submit" variant="primary">Confirm</Button>
+
+    </Container>
+</form>
+
+
+
+
   );
+
+  
+
 }
 export default App;
